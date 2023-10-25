01bda2edfc194bde52132e193fdfcdbb29c61d84
Alan Robson <alanr@live.com.au>
2023-10-25 17:50:34 +1100

    Add find_artist script.

---
+++ find_artist.py
@@ -0,0 +1,34 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
+
+db = client["record-db"]
+
+artist_name = "Bob Dylan"
+
+artist_document = db["artists"].find_one({"name": artist_name})
+
+if artist_document:
+    artistid = artist_document["artistid"]
+else:
+    print(f"Artist '{artist_name}' not found in the 'artists' collection.")
+    artistid = None
+
+if artistid is not None:
+    records = (
+        db["records"]
+        .find({"artistid": artistid})
+        .sort([("recorded", pymongo.DESCENDING)])
+    )
+
+    album_names = [
+        [record["name"], record["recorded"], record["media"]] for record in records
+    ]
+
+    if album_names:
+        print(f"Albums by {artist_name}:")
+
+        for album_name in album_names:
+            print(f"\t{album_name[1]}: {album_name[0]} ({album_name[2]})")
+    else:
+        print(f"No albums found for {artist_name}.")
