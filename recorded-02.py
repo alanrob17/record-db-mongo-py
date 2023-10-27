534282ae0b9f83b2968b1c54147622ec8cb0b0e3
Alan Robson <alanr@live.com.au>
2023-10-27 20:14:30 +1100

    Add more fields to recorded-02.

--- recorded-02.py
+++ recorded-02.py
@@ -2,13 +2,14 @@ from pymongo import MongoClient
 
 """
 Search for albums by Year using a dictionary.
+Same as recorded.py but uses dictionaries.
 """
 
 client = MongoClient("mongodb://localhost:27017")
 
 db = client["record-db"]
 
-recorded_year = 1973
+recorded_year = 1974
 records_by_year = db["records"].find({"recorded": recorded_year})
 
 artist_records_dict = {}
@@ -17,11 +18,12 @@ artist_records_dict = {}
 for record in records_by_year:
     artistid = record["artistid"]
     artist_name = db["artists"].find_one({"artistid": artistid})["name"]
-    record_name = record["name"]
 
     if artist_name not in artist_records_dict:
         artist_records_dict[artist_name] = []
-    artist_records_dict[artist_name].append(record_name)  # record_name
+    artist_records_dict[artist_name].append(
+        {"name": record["name"], "media": record["media"]}
+    )
 
 print(f"\nArtists with albums recorded in {recorded_year}:\n")
 
@@ -31,9 +33,8 @@ for artist_name in artist_records_dict:
 
     if artist_records:
         for record in artist_records:
-            record_name = record
-            print(f"\t\t{record_name}")
+            print(f"\t\t{record['name']} ({record['media']})")
     else:
-        print("No records for this artist in 1974")
+        print(f"No records for this artist in {recorded_year}")
 
 print()
